8895e867b7fb0b101092290a5c16754895bc0d86
Kauan Matheus Sanche Corrêa <kauan.math.correa@gmail.com>
2025-10-03 11:19:26 -0300

    Add files via upload

---
+++ Front/script.js
@@ -0,0 +1,36 @@
+const resultsDiv = document.getElementById("results")
+
+
+async function fetchFruits(){
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+        console.log(data)
+
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Erro ao buscar dados!</p>"
+            return
+        }
+        resultsDiv.innerHTML = ''
+
+        data.forEach(item => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${item.imagem}" alt="${item.nome}"
+                <h2>${item.nome}</h2>
+                <h3>${item.descricao}</h3>
+                <p>R$${item.preco.toFixed(2)}</p>
+            `
+            resultsDiv.appendChild(card)
+        });
+
+    } catch (error) {
+        resultsDiv.innerHTML = "<p>Erro ao buscar produtos!</p>"
+    }
+
+}
+
+fetchFruits()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Forja-manha-BD-01
+Tags html-exemplos:
+body
+div
+h1
+h2
+h3
+p
+class-atributo
+id
